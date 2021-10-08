4a84a74351872cc7203a8472959b2f2bf845a269
KelvinS <kelvinpfw@gmail.com>
2021-10-08 18:34:51 -0300

    Start creating crypto_test

--- pkg/crypto/crypto_test.go
+++ pkg/crypto/crypto_test.go
@@ -1,1 +1,14 @@
 package crypto
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEncryptDecrypt(t *testing.T) {
+	key := "a1a2a3a4a5a6a7a8"
+	text := "The quick brown fox jumps over the lazy dog"
+	encryptedText := Encrypt(key, text)
+	decryptedText := Decrypt(key, encryptedText)
+	assert.Equal(t, text, decryptedText, "texts should be equal")
+}
