44824ab1fad67d931830a79f33371fe15d9fc3e4
KelvinS <kelvinpfw@gmail.com>
2021-10-19 14:55:56 -0300

    Implement credential test

--- internal/models/credential_test.go
+++ internal/models/credential_test.go
@@ -1,1 +1,85 @@
 package models
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"log"
+	"fmt"
+	"testing"
+)
+
+func setUp() {
+	os.Setenv("PASSAGER_DATABASE", "/tmp/passager-test.db")
+}
+
+func tearDown() {
+	os.Remove("/tmp/passager-test.db")
+}
+
+func assertEqualCredential(t *testing.T, first, second Credential) {
+	assert.Equal(t, first.Name, second.Name, "name should be equal")
+	assert.Equal(t, first.Login, second.Login, "login should be equal")
+	assert.Equal(t, first.Password, second.Password, "password should be equal")
+	assert.Equal(t, first.Description, second.Description, "description should be equal")
+}
+
+func TestCreate(t *testing.T) {
+	setUp()
+	defer tearDown()
+	credential := Credential{
+		Name:        "name",
+		Login:       "login",
+		Password:    "password",
+		Description: "description",
+	}
+	Create(&credential)
+
+	conn := openConnection()
+	var loadedCredential Credential
+	conn.First(&loadedCredential, "name = name")
+	assertEqualCredential(t, credential, loadedCredential)
+}
+
+func TestReadAll(t *testing.T) {
+	setUp()
+	defer tearDown()
+	conn := openConnection()
+	credential := Credential{
+		Name:        "name",
+		Login:       "login",
+		Password:    "password",
+		Description: "description",
+	}
+	conn.Create(&credential)
+
+	credentials, err := ReadAll("name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, len(credentials), 1, "length should be one")
+	assertEqualCredential(t, credential, credentials[0])
+}
+
+func TestDelete(t *testing.T) {
+	setUp()
+	defer tearDown()
+	conn := openConnection()
+	credential := Credential{
+		Name:        "name",
+		Login:       "login",
+		Password:    "password",
+		Description: "description",
+	}
+	conn.Create(&credential)
+
+	var loadedCredential Credential
+	conn.First(&loadedCredential, "name = name")
+	assertEqualCredential(t, credential, loadedCredential)
+
+	err := Delete("name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = conn.First(&loadedCredential, "name = name").Error
+	assert.Equal(t, fmt.Sprintf("%s", err), "record not found", "message should be record not found")
+}
