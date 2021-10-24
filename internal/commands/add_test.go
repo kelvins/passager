b8049409e394eaf3dd8c3139e4851d809a3666b5
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-10-24 16:45:40 -0300

    Add tests to the add command

--- internal/commands/add_test.go
+++ internal/commands/add_test.go
@@ -1,1 +1,39 @@
 package commands
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/kelvins/passager/internal/models"
+	"github.com/kelvins/passager/pkg/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+func setUp() {
+	os.Setenv("PASSAGER_DATABASE", "/tmp/passager-test.db")
+}
+
+func tearDown() {
+	os.Unsetenv("PASSAGER_DATABASE")
+	os.Remove("/tmp/passager-test.db")
+}
+
+func TestAddCmd(t *testing.T) {
+	setUp()
+	defer tearDown()
+	cmd := AddCmdFactory()
+	buf := bytes.NewBufferString("")
+	cmd.SetOut(buf)
+	cmd.SetArgs([]string{"FOO", "BAR", "PASS"})
+	cmd.Execute()
+	out, _ := ioutil.ReadAll(buf)
+	assert.Equal(t, "Credential FOO successfully created!\n", string(out))
+	cred, err := models.ReadAll("FOO")
+	assert.Nil(t, err)
+	assert.Equal(t, len(cred), 1)
+	assert.Equal(t, "FOO", cred[0].Name)
+	assert.Equal(t, "BAR", cred[0].Login)
+	assert.Equal(t, "PASS", crypto.Decrypt(cred[0].Password, crypto.EncryptionKey()))
+}
