8ce75715b5820fdb0d8ae432fe4c48c18a5564cd
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-10-24 16:45:09 -0300

    Fix root command test

--- internal/commands/root_test.go
+++ internal/commands/root_test.go
@@ -3,9 +3,10 @@ package commands
 import (
 	"bytes"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRootCmd(t *testing.T) {
@@ -15,5 +16,5 @@ func TestRootCmd(t *testing.T) {
 	cmd.SetArgs([]string{"--version"})
 	cmd.Execute()
 	out, _ := ioutil.ReadAll(buf)
-	assert.Equal(t, string(out), fmt.Sprintf("Passager v%s", Version))
+	assert.Equal(t, string(out), fmt.Sprintf("Passager v%s\n", Version))
 }
