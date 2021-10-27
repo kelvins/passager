e8d2601f56d246db8ea018ab6cbc6edefdbb367e
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-10-26 22:58:44 -0300

    Add tests to the get command

--- internal/commands/get_test.go
+++ internal/commands/get_test.go
@@ -1,1 +1,40 @@
 package commands
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/kelvins/passager/internal/models"
+	"github.com/kelvins/passager/pkg/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCmd(t *testing.T) {
+	setUp()
+	defer tearDown()
+
+	cred, err := models.ReadAll("FOO")
+	assert.Nil(t, err)
+	assert.Equal(t, len(cred), 0)
+
+	credential := models.Credential{
+		Name:        "FOO",
+		Login:       "BAR",
+		Password:    crypto.Encrypt("PASS", crypto.EncryptionKey()),
+		Description: "DESC",
+	}
+	models.Create(&credential)
+
+	cmd := GetCmdFactory()
+	buf := bytes.NewBufferString("")
+	cmd.SetOut(buf)
+	cmd.SetArgs([]string{"FOO"})
+	cmd.Execute()
+	out, _ := ioutil.ReadAll(buf)
+
+	replacer := strings.NewReplacer("-", "", "+", "", " ", "", "\n", "")
+	expectedResult := "|NAME|LOGIN|PASSWORD|DESCRIPTION||FOO|BAR|PASS|DESC|"
+	assert.Equal(t, expectedResult, replacer.Replace(string(out)))
+}
